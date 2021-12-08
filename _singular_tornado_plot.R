2c69e5b383ee950a4fc446f15f0589d43bae492b
Abad <n795d@odcf-worker01.inet.dkfz-heidelberg.de>
2021-12-08 15:51:48 +0100

    adding functions to work properly; tested

---
+++ _singular_tornado_plot.R
@@ -0,0 +1,118 @@
+# dependencies.packages = c('ggplot2', 'data.table', 'devtools','grid', 'gridExtra','tiff',"shiny","shinydashboard","entropy")
+
+# install.packages(dependencies.packages)
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+# install.packages("BiocManager")
+
+# BiocManager::install(c('GenomicRanges','quantsmooth','IRanges'))
+# devtools::install()
+library("optparse")
+
+args = commandArgs(trailingOnly=TRUE)
+
+option_list = list(
+  make_option(c("-c", "--chromosome"), type="character", default=NULL,
+              help="Chromosome", metavar="character"),
+  make_option(c("-g", "--gene"), type="character", default=NULL,
+              help="Gene", metavar="character"),
+  make_option(c("-f", "--folder"), type="character", default="/omics/groups/OE0436/internal/nabad/GenomeTornadoPlot/PCAWG_plots_16Nov",
+              help="Folder of Plots", metavar="character"),
+  make_option(c("-s", "--saveplots"), type="character", default="/omics/groups/OE0436/internal/nabad/GenomeTornadoPlot/PCAWG_plots_16Nov",
+              help="Decide to save plots or not.", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+if (is.null(opt$chromosome) || is.null(opt$gene)){
+  stop("Please specify both the chromosome and the gene.")
+} else {
+  print(paste("Chromosome: ", opt$chromosome))
+  print(paste("Gene: ", opt$gene))
+  print(paste("Output Folder: ", opt$folder))
+
+  library(ggplot2)
+  library(GenomeTornadoPlot)
+
+  get_tornado_plot <- function(
+    chromosome,
+    gene_name,
+    folder_of_plots
+  ) {
+    # Turn chromosome and gene_name to strings.
+    chromosome <- paste(chromosome)
+    gene_name <- paste(gene_name)
+
+    # Load the dataset.
+    path_to_rdata <- paste("/omics/groups/OE0436/internal/nabad/GenomeTornadoPlot/GenomeTornadoPlot-files/chr", chromosome, ".Rdata", sep="")
+    load(path_to_rdata)
+
+    # Rename columns.
+    chr <- cnv_chr
+    names(chr)[4] <- "Score"
+    names(chr)[6] <- "Cohort"
+    names(chr)[7] <- "PID"
+
+    # MakeData that is necessary for generating TornadoPlots
+    sdt <- MakeData(
+      CNV=chr,
+      gene_name_1=gene_name,
+      score.type="del",
+      gene_score_1=30
+    )
+
+    # Generate plots.
+    not_zoomed <- TornadoPlots(
+      sdt,
+      color.method="ploidy",
+      sort.method="length",
+      multi_panel=FALSE,
+      font.size.factor=1.5,
+      orient="v",
+      zoomed="global"
+    )
+
+    zoomed <- TornadoPlots(
+      sdt,
+      color.method="ploidy",
+      sort.method="length",
+      multi_panel=FALSE,
+      font.size.factor=1.5,
+      orient="v",
+      zoomed="region"
+    )
+
+    # Wait 5 seconds for everything to load properly.
+    Sys.sleep(5)
+
+    # Save plots.
+    path_to_not_zoomed <- paste(
+      folder_of_plots, "/chr", chromosome, "_", gene_name, "_not_zoomed.png",
+      sep=""
+    )
+
+    path_to_zoomed <- paste(
+      folder_of_plots, "/chr", chromosome, "_", gene_name, "_zoomed.png",
+      sep=""
+    )
+
+    ggsave(
+      file=path_to_not_zoomed,
+      grid.arrange(not_zoomed[[2]])
+    )
+
+    ggsave(
+      file=path_to_zoomed,
+      grid.arrange(zoomed[[2]])
+    )
+
+  }
+
+  get_tornado_plot(
+    chromosome=opt$chromosome,
+    gene_name=opt$gene,
+    folder_of_plots=opt$folder
+  )
+  print("... finished.")
+}
---
+++ main.py
@@ -0,0 +1,140 @@
+import os
+import pandas as pd
+from optparse import OptionParser
+import shutil
+import glob
+import time
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option(
+        "-p",
+        "--path",
+        action="store",
+        type="str",
+        dest="path",
+        help="Path to the CSV file. \n",
+    )
+
+    parser.add_option(
+        "-o",
+        "--output-folder",
+        action="store",
+        type="str",
+        dest="output_folder",
+        help="Path to the output folder. \n",
+    )
+
+    parser.add_option(
+        "-r",
+        "--rscript",
+        action="store",
+        default="/home/n795d/workspace/genome-tornado-plot-wrapper/_singular_tornado_plot.R",
+        type="str",
+        dest="rscript",
+        help="Path to the _singular_tornado_plot.R script. \n",
+    )
+
+    (options, args) = parser.parse_args()
+    start_time = time.time()
+    try:
+        data = pd.read_csv(options.path)
+    except:
+        data = pd.read_csv(options.path, delimiter = ";")
+
+    output_folder = options.output_folder
+
+    for chromosome in data["#CHROM"].unique():
+        if not os.path.exists(os.path.join(output_folder, chromosome)):
+            os.mkdir(os.path.join(output_folder, chromosome))
+
+    # Create folder of temporary .sh scripts.
+    folder_of_sh_scripts = os.path.join(output_folder, "temp_sh_scripts")
+    if os.path.exists(folder_of_sh_scripts):
+        shutil.rmtree(folder_of_sh_scripts)
+    os.mkdir(folder_of_sh_scripts)
+
+    # Create .sh scripts
+    png_files_to_create = []
+    print("Creating sh scripts...")
+    for idx, row in data.iloc[:500].iterrows():
+        chromosome = row["#CHROM"]
+        gene = row["GENE"]
+
+        path_to_sh_script = os.path.join(folder_of_sh_scripts, f"{str(idx)}.sh")
+        with open(path_to_sh_script, "w") as f:
+            f.write("module load R/4.0.0 \n")
+            f.write(f"Rscript {options.rscript} --chromosome {chromosome} --gene {gene} --folder {os.path.join(output_folder, chromosome)}")
+        
+        png_file = os.path.join(output_folder, chromosome, f"chr{chromosome}_{gene}_not_zoomed.png")
+        png_files_to_create.append((png_file, path_to_sh_script))
+
+
+    # Call each of the .sh scripts using the bsub function.
+    print("Calling sh scripts...")
+    for sh_script in glob.glob(os.path.join(folder_of_sh_scripts, "*")):
+        sh_scriptname = sh_script.split("/")[-1]
+
+        num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+        num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+        num_total_jobs = num_running_jobs + num_pending_jobs
+
+        # Only send 100 jobs to the cluster at a time.
+        # If more than 100 jobs, wait until there are less than 100 jobs running.
+        while num_total_jobs > 50:
+            num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+            num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+            num_total_jobs = num_running_jobs + num_pending_jobs
+            print(f"{num_total_jobs} total jobs. Sleeping 15 seconds and trying again.")
+            time.sleep(15)
+
+        # Call the command.
+        command = f"bsub -J {sh_scriptname} -q medium -W 1:00 -M 10GB -R rusage[mem=10GB] < {sh_script}"
+        os.popen(command)
+
+    # Wait until there are no more running/pending jobs to check for errors.
+    num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+    num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+    num_total_jobs = num_running_jobs + num_pending_jobs
+
+    while num_total_jobs != 0:
+        print(f"Waiting for remaining jobs to finish... {num_total_jobs} remaining.")
+        num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+        num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+        num_total_jobs = num_running_jobs + num_pending_jobs
+        time.sleep(10)        
+
+    # Check which png files have not yet been created.
+    remaining_files = [png_file_and_sh_script for png_file_and_sh_script in png_files_to_create if not os.path.exists(png_file_and_sh_script[0])]
+    iteration = 0
+    while len(remaining_files) != 0:
+        print(f"Number of Remaining Files in Iteration {iteration}: {len(remaining_files)}")
+        # Call the sh scripts that are still remaining.
+        for png_file_and_sh_script in remaining_files:
+            png_file, sh_script = png_file_and_sh_script
+            sh_scriptname = sh_script.split("/")[-1]
+
+            # Call the command.
+            command = f"bsub -J {sh_scriptname}_iter{iteration} -q medium -W 1:00 -M 10GB -R rusage[mem=10GB] < {sh_script}"
+            os.popen(command)
+
+        # Wait until the number of jobs is 0 to re-try these scripts.
+        num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+        num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+        num_total_jobs = num_running_jobs + num_pending_jobs
+        while num_total_jobs != 0:
+            num_running_jobs = int(os.popen("bjobs -r | wc -l").read())
+            num_pending_jobs = int(os.popen("bjobs -p | wc -l").read())
+            num_total_jobs = num_running_jobs + num_pending_jobs
+            time.sleep(10)
+
+        # Update the remaining files.
+        remaining_files = [png_file_and_sh_script for png_file_and_sh_script in png_files_to_create if not os.path.exists(png_file_and_sh_script[0])]
+        iteration += 1
+
+    print("Deleting sh scripts...")
+    shutil.rmtree(folder_of_sh_scripts)
+    print(f"DONE! Time Taken: {time.time() - start_time}")
+
+        
+
